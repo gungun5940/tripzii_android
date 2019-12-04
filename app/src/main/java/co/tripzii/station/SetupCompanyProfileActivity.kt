97dadb8242be80a44b483d1f88312f5c6a90e140
gungun5940 <surinthorngun@gmail.com>
2019-12-04 15:12:22 +0700

    gun - setup-company-profile

--- app/src/main/java/co/tripzii/station/SetupCompanyProfileActivity.kt
+++ app/src/main/java/co/tripzii/station/SetupCompanyProfileActivity.kt
@@ -8,5 +8,8 @@ class SetupCompanyProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup_company_profile)
+
+
+        
     }
 }
