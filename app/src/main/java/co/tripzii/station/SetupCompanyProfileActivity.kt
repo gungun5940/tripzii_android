8c97d18d1c497a4fd9d15adedc219964f9139843
gungun5940 <surinthorngun@gmail.com>
2019-12-12 13:11:35 +0700

    Gun - add dropdown

--- app/src/main/java/co/tripzii/station/SetupCompanyProfileActivity.kt
+++ app/src/main/java/co/tripzii/station/SetupCompanyProfileActivity.kt
@@ -2,6 +2,8 @@ package co.tripzii.station
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_setup_company_profile.*
 
 class SetupCompanyProfileActivity : AppCompatActivity() {
 
@@ -9,5 +11,12 @@ class SetupCompanyProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup_company_profile)
         supportActionBar?.title = "Setup Company Profile"
+        var province = resources.getStringArray(R.array.province)
+        var adapter = ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,province)
+        select_province_autoCompleteTextView.threshold=0
+        select_province_autoCompleteTextView.setAdapter(adapter)
+        select_province_autoCompleteTextView.setOnFocusChangeListener(
+            { view, b -> if (b) select_province_autoCompleteTextView.showDropDown()})
+
     }
 }
