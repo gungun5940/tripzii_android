745db2e1c6e04abed7a9d8b679f4e64bef48eadb
Tarn_se59 <Npwtps14_0720@hotmail.com>
2019-12-13 23:51:14 +0700

    fix

---
+++ app/src/main/java/co/tripzii/station/adapter/ReportCommissionDAO.kt
@@ -0,0 +1,8 @@
+package co.tripzii.station.adapter
+
+data class ReportCommissionDAO (
+    val name: String = "",
+    val date: String = "",
+    val price: String = "",
+    val total: String = ""
+)
