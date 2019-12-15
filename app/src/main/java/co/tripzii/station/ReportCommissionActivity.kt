d349973bb8e1f9c7269495f26a660a5c80f7e75b
Tarn_se59 <Npwtps14_0720@hotmail.com>
2019-12-16 03:48:47 +0700

    tarn - save wait for merge

--- app/src/main/java/co/tripzii/station/ReportCommissionActivity.kt
+++ app/src/main/java/co/tripzii/station/ReportCommissionActivity.kt
@@ -2,18 +2,27 @@ package co.tripzii.station
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telecom.Call
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.tripzii.station.adapter.ReportCommissionAdapter
+import co.tripzii.station.model.ReportCommissionDAO
+import com.google.android.gms.common.api.Response
 import kotlinx.android.synthetic.main.activity_report_commission.*
 
 
 class ReportCommissionActivity : AppCompatActivity() {
 
+    lateinit var adapter: ReportCommissionAdapter
+    lateinit var action: View.OnClickListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report_commission)
         supportActionBar?.title = "Report commission"
+
 
         val mount = resources.getStringArray(R.array.month_arrays)
 
@@ -23,22 +32,31 @@ class ReportCommissionActivity : AppCompatActivity() {
             android.R.layout.simple_spinner_item, // Layout
             mount // Array
         )
-        // Set the drop down view resource
+
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
 
-        // Finally, data bind the spinner object with dapter
         spinner.adapter = adapter;
 
-        // Set an on item selected listener for spinner object
         spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent:AdapterView<*>, view: View, position: Int, id: Long){
-                // Display the selected item text on text view
 
             }
 
             override fun onNothingSelected(parent: AdapterView<*>){
-                // Another interface callback
             }
+
+
         }
+
+
+    }
+
+//    private fun fetchMovies(){
+//        refreshLayout.isRefreshing = true
+
+
+        private fun showComission(Commission: List<ReportCommissionDAO>) {
+        recyclerViewCommission.layoutManager = LinearLayoutManager(this)
+        recyclerViewCommission.adapter = ReportCommissionAdapter(Commission)
     }
 }
--- app/src/main/java/co/tripzii/station/adapter/ReportCommissionAdapter.kt
+++ app/src/main/java/co/tripzii/station/adapter/ReportCommissionAdapter.kt
@@ -1,4 +1,36 @@
 package co.tripzii.station.adapter
 
-class ReportCommissionAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import co.tripzii.station.R
+import co.tripzii.station.model.ReportCommissionDAO
+import kotlinx.android.synthetic.main.list_commission.view.*
+
+class ReportCommissionAdapter ( val dataCommision : List<ReportCommissionDAO>) : RecyclerView.Adapter<ReportCommissionAdapter.CommissionViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommissionViewHolder {
+        return CommissionViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_commission, parent, false)
+        )
+    }
+    override fun getItemCount() = dataCommision.size
+
+
+    override fun onBindViewHolder(holder: CommissionViewHolder, position: Int) {
+        holder.itemView.setOnClickListener{
+            //do sth in here
+        }
+
+        holder.itemView.trip_name.text = this.dataCommision[position].name
+        holder.itemView.trip_date.text = this.dataCommision[position].date
+        holder.itemView.trip_price.text = this.dataCommision[position].price
+        holder.itemView.trip_total_com_price.text = this.dataCommision[position].total
+
+    }
+
+    class CommissionViewHolder( view: View) : RecyclerView.ViewHolder(view)
 }
+
