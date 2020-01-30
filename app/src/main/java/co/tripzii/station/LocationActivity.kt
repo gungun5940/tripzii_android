24b6e106af5e8b036da125e73add578c1af3085e
Tarn_se59 <Npwtps14_0720@hotmail.com>
2020-01-30 22:28:15 +0700

    tarn - save

--- app/src/main/java/co/tripzii/station/LocationActivity.kt
+++ app/src/main/java/co/tripzii/station/LocationActivity.kt
@@ -1,12 +1,31 @@
 package co.tripzii.station
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class LocationActivity : AppCompatActivity() {
+
+class BasicMapDemoActivity :
+    AppCompatActivity(),
+    OnMapReadyCallback {
+
+    val LocationPin = LatLng(-33.862, 151.21)
+    val ZOOM_LEVEL = 13f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location)
+        val mapFragment : SupportMapFragment? =
+            supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+    override fun onMapReady(googleMap: GoogleMap?) {
+        googleMap ?: return
+        with(googleMap) {
+            moveCamera(CameraUpdateFactory.newLatLngZoom(LocationPin, ZOOM_LEVEL))
+            addMarker(MarkerOptions().position(LocationPin))
+        }
     }
 }
