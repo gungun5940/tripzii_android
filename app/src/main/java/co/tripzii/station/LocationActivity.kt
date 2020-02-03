abfe5fe2adaa1f1430a9e3d99602873bbe83dc6d
Tarn_se59 <Npwtps14_0720@hotmail.com>
2020-02-03 22:16:49 +0700

    tarn - save

--- app/src/main/java/co/tripzii/station/LocationActivity.kt
+++ app/src/main/java/co/tripzii/station/LocationActivity.kt
@@ -2,30 +2,15 @@ package co.tripzii.station
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.GoogleMapOptions
 
 
-class BasicMapDemoActivity :
-    AppCompatActivity(),
-    OnMapReadyCallback {
-
-    val LocationPin = LatLng(-33.862, 151.21)
-    val ZOOM_LEVEL = 13f
+class LocationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location)
-        val mapFragment : SupportMapFragment? =
-            supportFragmentManager.findFragmentById(R.id.locationMapView) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
-    }
-    override fun onMapReady(googleMap: GoogleMap?) {
-        googleMap ?: return
-        with(googleMap) {
-            moveCamera(CameraUpdateFactory.newLatLngZoom(LocationPin, ZOOM_LEVEL))
-            addMarker(MarkerOptions().position(LocationPin))
-        }
+
+        val options = GoogleMapOptions().liteMode(true)
     }
 }
--- app/src/main/java/co/tripzii/station/TripDetailsActivity.kt
+++ app/src/main/java/co/tripzii/station/TripDetailsActivity.kt
@@ -1,18 +1,19 @@
 package co.tripzii.station
 
+//import co.tripzii.station.adapter.TripDetailsAdapter
+
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import co.tripzii.station.adapter.ReportCommissionAdapter
 import co.tripzii.station.adapter.TimelineAdapter
-//import co.tripzii.station.adapter.TripDetailsAdapter
-import co.tripzii.station.model.ReportCommissionDAO
 import co.tripzii.station.model.TimelineDAO
 import co.tripzii.station.model.TripDeailsDAO
-import kotlinx.android.synthetic.main.activity_report_commission.*
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_trip_details.*
 
 class TripDetailsActivity : AppCompatActivity() {
@@ -20,11 +21,50 @@ class TripDetailsActivity : AppCompatActivity() {
     lateinit var adapter: TimelineAdapter
 //    lateinit var adapter2: TripDetailsAdapter
     lateinit var action: View.OnClickListener
+    private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trip_details)
+
+        val tripNameDetails = findViewById(R.id.tripNameDetailsTextView) as TextView
+        val tripRate = findViewById(R.id.tripRateTextView) as TextView
+        val tripPrice = findViewById(R.id.tripPriceTextView) as TextView
+
+
+
+        db.collection("trip")
+            .document("01")
+            .collection("type")
+            .document("001")
+            .collection("trips")
+            .addSnapshotListener { querySnapshot, error ->
+                val document = querySnapshot?.documentChanges
+                for (doc in document!!)  {
+                    Log.d("DocumentSnapshot", doc.toString())
+                }
+            }
+
+
+//        val docRef = db.collection("trip").document("1").collection("Lampang")
+//        docRef.get()
+//            .addOnSuccessListener { document ->
+//                if (document != null) {
+//                    Log.d("1", "c data: ${document}")
+//
+////                    tripNameDetails.text = document.getString("title")
+////                    tripRate.text = document.getString("score")
+////                    tripPrice.text = document.getString("price")
+//
+//                } else {
+//                    Log.d("2", "No such document")
+//                }
+//            }
+//            .addOnFailureListener { exception ->
+//                Log.d("3", "get failed with ", exception)
+//            }
+
 
         val reportComs = ArrayList<TimelineDAO>()
         reportComs.add(TimelineDAO("7:00  am", "Pick up at the hotel"))
@@ -37,29 +77,29 @@ class TripDetailsActivity : AppCompatActivity() {
         val reportAdapter = TimelineAdapter(reportComs)
         timelineRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
         timelineRecyclerView.adapter = reportAdapter
-        val reportTripDetails = TripDeailsDAO(
-            "Doi intanon ",
-            "Chaing mai ",
-            "4.2",
-            "5000",
-            "3",
-            "Wachira Tarn Waterfall Siritarn Waterfall Doi Inthanon – The highest peak in Thailand King and Queen’s PagodasHmong marketKarenhilltribe village",
-            "dairy trip",
-            "full day",
-            "group",
-            "trip incllude",
-            "trip remark")
-        tripNameDetailsTextView.text = reportTripDetails.tripNameDetail
-        tripDetailsLocationTextView.text = reportTripDetails.triplocation
-        tripRateTextView.text = reportTripDetails.triprating
-        tripPriceTextView.text = reportTripDetails.tripprice
-        tripDayTextView.text = reportTripDetails.tripDay
-        descDetailsTextView.text = reportTripDetails.tripDesc
-        scheduleTextView.text = reportTripDetails.tripSchedule
-        durationTextView.text = reportTripDetails.duration
-        typeTextView.text = reportTripDetails.tripType
-        includeTextView.text = reportTripDetails.tripInclude
-        remarkTextView.text = reportTripDetails.tripRemark
+//        val reportTripDetails = TripDeailsDAO(
+//            "Doi intanon ",
+//            "Chaing mai ",
+//            "4.2",
+//            "5000",
+//            "3",
+//            "Wachira Tarn Waterfall Siritarn Waterfall Doi Inthanon – The highest peak in Thailand King and Queen’s PagodasHmong marketKarenhilltribe village",
+//            "dairy trip",
+//            "full day",
+//            "group",
+//            "trip incllude",
+//            "trip remark")
+//        tripNameDetailsTextView.text = reportTripDetails.tripNameDetail
+//        tripDetailsLocationTextView.text = reportTripDetails.triplocation
+//        tripRateTextView.text = reportTripDetails.triprating
+//        tripPriceTextView.text = reportTripDetails.tripprice
+//        tripDayTextView.text = reportTripDetails.tripDay
+//        descDetailsTextView.text = reportTripDetails.tripDesc
+//        scheduleTextView.text = reportTripDetails.tripSchedule
+//        durationTextView.text = reportTripDetails.duration
+//        typeTextView.text = reportTripDetails.tripType
+//        includeTextView.text = reportTripDetails.tripInclude
+//        remarkTextView.text = reportTripDetails.tripRemark
 
     }
 }
