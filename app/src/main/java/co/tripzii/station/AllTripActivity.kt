849a0d70bb34b5c90581c178865b0c553c67e672
gungun5940 <surinthorngun@gmail.com>
2020-02-19 17:52:03 +0700

    Gun - save

--- app/src/main/java/co/tripzii/station/AllTripActivity.kt
+++ app/src/main/java/co/tripzii/station/AllTripActivity.kt
@@ -10,6 +10,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
@@ -20,6 +26,7 @@ import co.tripzii.station.model.TripModel
 import co.tripzii.station.ui.activities.ActivitiesFragment
 import co.tripzii.station.ui.ticket.TicketFragment
 import co.tripzii.station.ui.transfer.TransferFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
 import com.viewpagerindicator.CirclePageIndicator
@@ -36,19 +43,9 @@ class AllTripActivity : AppCompatActivity() {
 
     lateinit var drawerLayout: DrawerLayout
 
-    private var imageModelArrayList: ArrayList<ImageModel>? = null
-
-    private val myImageList = intArrayOf(R.drawable.img_doiinthanon, R.drawable.img_mist,
-        R.drawable.img_sea, R.drawable.img_temple, R.drawable.img_phiphi_island)
-
-    private val db = FirebaseFirestore.getInstance()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_trip)
-        imageModelArrayList = ArrayList()
-        imageModelArrayList = populateList()
-        init()
         setHamburgerButton()
         homeMenuTextView.setOnClickListener {
             val intent = Intent(this, AllTripActivity::class.java)
@@ -80,23 +77,15 @@ class AllTripActivity : AppCompatActivity() {
             val intent = Intent(this, HotelAccountActivity::class.java)
             startActivity(intent)
         }
-        navBottomNavigation.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_activities -> {
-                    replaceFragment(ActivitiesFragment())
-                    return@setOnNavigationItemSelectedListener true
-                }
-                R.id.navigation_ticket -> {
-                    replaceFragment(TicketFragment())
-                    return@setOnNavigationItemSelectedListener true
-                }
-                R.id.navigation_transfer -> {
-                    replaceFragment(TransferFragment())
-                    return@setOnNavigationItemSelectedListener true
-                }
-            }
-            false
-        }
+        val navView: BottomNavigationView = findViewById(R.id.navBottomNavigation)
+        val navController = findNavController(R.id.navFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                    R.id.navigation_activities,R.id.navigation_ticket,R.id.navigation_transfer
+            )
+        )
+        appBarConfiguration.fallbackOnNavigateUpListener
+        navView.setupWithNavController(navController)
     }
 
     private fun setHamburgerButton() {
@@ -118,124 +107,11 @@ class AllTripActivity : AppCompatActivity() {
             dialog.show() }
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
-
-    private fun populateList(): ArrayList<ImageModel> {
-        val list = ArrayList<ImageModel>()
-        for (i in 0..4) {
-            val imageModel = ImageModel()
-            imageModel.setImageDrawables(myImageList[i])
-            list.add(imageModel)
-        }
-        return list
-    }
-
-    private fun init() {
-        viewPage = findViewById(R.id.viewPager)
-        viewPage!!.adapter = SliderImageAdapter(this, imageModelArrayList!!)
-        val indicator = findViewById(R.id.indicator) as CirclePageIndicator
-        indicator.setViewPager(viewPager)
-        val density = resources.displayMetrics.density
-        indicator.setRadius(4 * density)
-        NumPage = imageModelArrayList!!.size
-        val handler = Handler() // Auto start of viewpager
-        val update = Runnable {
-            if (currentPage == NumPage) {
-                currentPage = 0
-            }
-            viewPage!!.setCurrentItem(currentPage++, true)
-        }
-        val swipeTimer = Timer()
-        swipeTimer.schedule(object : TimerTask() {
-            override fun run() {
-                handler.post(update)
-            }
-        }, 3000, 3000)
-        indicator.setOnPageChangeListener(object : ViewPager.OnPageChangeListener { // Pager listener over indicator
-
-            override fun onPageSelected(position: Int) {
-                currentPage = position
-            }
-
-            override fun onPageScrolled(pos: Int, arg1: Float, arg2: Int) {
-            }
-
-            override fun onPageScrollStateChanged(pos: Int) {
-            }
-        })
-    }
-
-    companion object {
-        private var viewPage: ViewPager? = null
-        private var currentPage = 0
-        private var NumPage = 0
-    }
-
     override fun onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        val trip: MutableList<TripModel> = mutableListOf()
-        val getData = db.collection("alltrip")
-        getData.get()
-            .addOnCompleteListener { task ->
-                for (ds in task.result?.documentChanges!!) {
-                    val item = ds.document.toObject(TripModel::class.java).apply {
-                        tripId = ds.document.id
-                    }
-                    when (ds.type) {
-                        DocumentChange.Type.ADDED -> {
-                            val items = trip.filter { it.tripId == item.tripId }
-                            if (items.isEmpty()) item.let { trip.add(it) }
-                        }
-                        DocumentChange.Type.MODIFIED -> {
-                            val index = trip.indexOfFirst { it.tripId == item.tripId }
-                            item.apply { trip[index] = this }
-                        }
-                        else -> {}
-                    }
-                }
-                val tripAdapter = TripAdapter(trip, onSelectItem = { trip ->
-                    Log.d("alltrip", trip.toString())
-                    val intent = Intent(this@AllTripActivity, TripDetailsActivity::class.java)
-                    intent.putExtra("trip", trip)
-                    startActivity(intent)
-                })
-                recyclerView.layoutManager = LinearLayoutManager(this,
-                    RecyclerView.HORIZONTAL, false)
-                recyclerView.adapter = tripAdapter
-                tripAdapter.notifyDataSetChanged()
-                val tripPopularAdapter = TripAdapter(trip, onSelectItem = { trip ->
-                    Log.d("PopularTrip", trip.toString())
-                    val intent = Intent(this@AllTripActivity, TripDetailsActivity::class.java)
-                    intent.putExtra("trip", trip)
-                    startActivity(intent)
-                })
-                popularRecyclerView.layoutManager = LinearLayoutManager(this,
-                    RecyclerView.HORIZONTAL, false)
-                popularRecyclerView.adapter = tripPopularAdapter
-                tripPopularAdapter.notifyDataSetChanged()
-                val tripRecommendedAdapter = TripAdapter(trip, onSelectItem = { trip ->
-                    Log.d("RecommendedTrip", trip.toString())
-                    val intent = Intent(this@AllTripActivity, TripDetailsActivity::class.java)
-                    intent.putExtra("trip", trip)
-                    startActivity(intent)
-                })
-                recommendedRecyclerView.layoutManager = LinearLayoutManager(this,
-                    RecyclerView.HORIZONTAL, false)
-                recommendedRecyclerView.adapter = tripRecommendedAdapter
-                tripRecommendedAdapter.notifyDataSetChanged()
-            }
     }
 }
